32a727f8c95be515c17a6636ab272d267704c082
mcgyver5 <tim.e.mcguire@gmail.com>
2013-11-22 07:39:13 -0600

    losing function

--- hashes_text.py
+++ hashes_text.py
@@ -21,6 +21,5 @@ parser.add_argument('--alg','-alg',default='md5',type=allowed_alg)
 parser.add_argument('--iterations','-it',default=1,type=int)
 parser.add_argument('-salt',default='')
 args = parser.parse_args()
-my_it_args = vars(parser.parse_args())
 
 print(args.alg + " hash of " + args.text + " is " +get_hash(args.text,args.salt,args.iterations,args.alg))
