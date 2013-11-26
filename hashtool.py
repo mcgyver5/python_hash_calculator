25b15b43b7e320d2e225c9a4df3cc52e47d7fb2f
mcgyver5 <tim.e.mcguire@gmail.com>
2013-11-25 18:36:04 -0600

    another function for one iteration

--- hashtool.py
+++ hashtool.py
@@ -6,13 +6,15 @@ class Hashtool():
     def __init__(self):
         pass
 
-    def get_single_hash(self,text,salt,iterations,algorithm):
+    def get_single_hash(self,text,salt,algorithm):
         salted = (text+salt).encode("utf-8")
         h = hashlib.new(algorithm)
         h.update(salted)
         return h.hexdigest()
 
     def get_hash(self, text, salt, iterations, algorithm):
+        if iterations == 1:
+            return self.get_single_hash(text,salt,algorithm)
         original = (text+salt).encode("utf-8")
         print("---" + salt.encode("utf-8"))
         for x in range(0,iterations):
